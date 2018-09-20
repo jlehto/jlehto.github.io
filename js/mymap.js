6838e0bd72a320095c9e33f1cff2249a883a0b7c
YarkoL <yarko.dev@gmail.com>
2018-09-20 17:13:01 +0300

    show position using geolocation api

--- js/mymap.js
+++ js/mymap.js
@@ -1,18 +1,28 @@
 
-var mymap;
+var myMap;
 var lyrOSM;
 
-var pos = [60.1699, 24.9384]; //hgin keskusta
-
 $(document).ready(function(){
-	
-
-	mymap = L.map('map', {center:pos, zoom:20});
-	lyrOSM = L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png');
-	mymap.addLayer(lyrOSM);
+	locateBtn.addEventListener("click", locate);
+	loadMap([60.1699, 24.9384]); //hgin keskusta
 });
   
+function loadMap(pos) {
+	myMap = L.map('map', {center:pos, zoom:15});
+	lyrOSM = L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png');
+	myMap.addLayer(lyrOSM);
+}  
 
-/*
+function locate() {
+	//console.log("Locate");
+	navigator.geolocation.getCurrentPosition(position => {
+		pos = [position.coords.latitude, position.coords.longitude];
+		myMap.off();
+		myMap.remove();
+	    loadMap(pos);
+	},error);
 
-*/  
+	function error(err) {
+	  console.warn(`ERROR(${err.code}): ${err.message}`);
+	}
+}
