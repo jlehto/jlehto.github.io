78fceccd3fa06bf7b2c3e64e8e82166a5384129d
YarkoL <yarko.dev@gmail.com>
2018-09-20 15:43:49 +0300

    first map

---
+++ js/mymap.js
@@ -0,0 +1,19 @@
+
+var mymap;
+var lyrOSM;
+
+var pos = [60.405373, 25.1028415];
+
+$(document).ready(function(){
+	mymap = L.map('map', {center:pos, zoom:30});
+	lyrOSM = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
+	mymap.addLayer(lyrOSM);
+});
+  
+
+/*
+navigator.geolocation.getCurrentPosition(position => {
+  console.log(position.coords.latitude, position.coords.longitude);
+});
+
+*/  
