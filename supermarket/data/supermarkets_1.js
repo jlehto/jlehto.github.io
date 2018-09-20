c482122bf998f8fe8165a6593e2b9fbdef56b9bd
YarkoL <yarko.dev@gmail.com>
2018-09-20 21:54:59 +0300

    qgis webmap

---
+++ supermarket/data/supermarkets_1.js
@@ -0,0 +1,13 @@
+var json_supermarkets_1 = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "Market": "Madeira", "Employees": 8.000000 }, "geometry": { "type": "Point", "coordinates": [ -122.429375, 37.756648 ] } },
+{ "type": "Feature", "properties": { "Market": "Bready Shop", "Employees": 15.000000 }, "geometry": { "type": "Point", "coordinates": [ -122.425337, 37.757819 ] } },
+{ "type": "Feature", "properties": { "Market": "Super River", "Employees": 25.000000 }, "geometry": { "type": "Point", "coordinates": [ -122.428816, 37.755875 ] } },
+{ "type": "Feature", "properties": { "Market": "Ben's Shop", "Employees": 10.000000 }, "geometry": { "type": "Point", "coordinates": [ -122.4317044, 37.7529726 ] } },
+{ "type": "Feature", "properties": { "Market": "Sanchez", "Employees": 12.000000 }, "geometry": { "type": "Point", "coordinates": [ -122.430028, 37.752131 ] } },
+{ "type": "Feature", "properties": { "Market": "Richvalley", "Employees": 20.000000 }, "geometry": { "type": "Point", "coordinates": [ -122.433229, 37.753511 ] } }
+]
+}
