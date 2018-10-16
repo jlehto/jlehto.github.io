d6b764975d7e90fc26b3f08e6c8073a317fc5468
YarkoL <yarko.dev@gmail.com>
2018-10-16 20:47:19 +0300

    coloring based on population

--- choropleth/index.js
+++ choropleth/index.js
@@ -16,8 +16,39 @@ $(document).ready(() => {
 
 const init = () => {
 	
-	map = L.map('map').setView([60.40, 25.07], 9);
+	map = L.map('map').setView([60.40, 25.07], 8);
 
 	L.tileLayer(tileUrl,tileOptions).addTo(map);
-	let geojson = L.geoJson(uusimaaData).addTo(map);
+	let geojson = L.geoJson(uusimaaData, {style: style}).addTo(map);
 }
+
+const style = feature => ({
+        fillColor: getColor(feature.properties.population),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '1',
+        fillOpacity: 0.5
+})
+
+const colors = ['#f7fcf5',
+				'#e5f5e0',
+				'#c7e9c0',
+				'#a1d99b',
+				'#74c476',
+				'#41ab5d',
+				'#238b45',
+				'#006d2c',
+				'#00441b'];
+
+const getColor = p =>
+	p > 500000 ? colors[8] :
+	p > 100000 ? colors[7] :
+	p > 50000  ? colors[6] :
+	p > 40000  ? colors[5] :
+	p > 30000  ? colors[4] :
+	p > 20000  ? colors[3] :
+	p > 10000  ? colors[2] :
+	p > 5000   ? colors[1] :
+				colors[0];
+
